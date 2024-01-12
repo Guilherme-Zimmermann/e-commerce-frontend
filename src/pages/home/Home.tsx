d3540b180b7d684eba42ff64121954189f13c340
GuiZimmermann <zimmerma83@gmail.com>
2024-01-11 20:38:09 -0400

    feat: home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import { Category} from "../../components/category/Category";
+import { Product } from "../../components/product/Product";
+
+
+export function Home() {
+    return (
+        <div>
+            <Category />
+            <Product />
+            <Product filtered="Piscina"/>
+        </div>
+    )
+}
