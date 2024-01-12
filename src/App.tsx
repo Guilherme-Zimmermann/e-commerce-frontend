679b7580b7d4fff82c041fc7d797a7fb07f8ce86
GuiZimmermann <zimmerma83@gmail.com>
2024-01-11 20:37:18 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
 import styles from "./App.module.css"
-import { Categories } from "./Components/Categories"
-import { Header } from "./Components/Header"
 
 function App() {
   return (
-    <div className={styles.container}>
-      <Header />
-        { /* Adicionar Slide de imagens */}
-      <Categories />
-    </div>
+    <BrowserRouter >
+      <div className={styles.container}>
+        <Router />
+      </div>
+    </BrowserRouter>
   )
 }
 
