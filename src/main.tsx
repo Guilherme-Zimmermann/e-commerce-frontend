0b7dbeea62b94c93d95a800bd236dfc516633795
GuiZimmermann <zimmerma83@gmail.com>
2024-02-02 14:32:00 -0400

    timer require

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from 'react-query'
 import App from './App.tsx'
@@ -12,6 +12,20 @@ export const supabase = createClient(
     'https://unejxaaiizvcupsiyxuy.supabase.co',
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuZWp4YWFpaXp2Y3Vwc2l5eHV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNjczNDY1NSwiZXhwIjoyMDIyMzEwNjU1fQ.ikNDaBMA04XBmrJVsTQ9Q2K3LMktiCWwQPm8kBRJbaY"
   )
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(baseUrl+"/api/product")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch((error) => {
+          console.error('Erro:', error);
+        });
+    }, 180000); // 180000 milissegundos equivalem a 3 minutos
+  
+    // Limpar intervalo quando o componente for desmontado
+    return () => clearInterval(interval);
+  }, []);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
